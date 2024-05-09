6874b9f80c246b37e144b7ef78a21dfba9430a07
Anthony Shin <shinn92dev@gmail.com>
2024-05-09 16:55:19 -0700

    Added javascript file - app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+const contentText = document.querySelector(".content");
+document.addEventListener(
+    "DOMContentLoaded",
+    function () {
+        contentText.innerHTML = "This is the content";
+    },
+    false
+);
